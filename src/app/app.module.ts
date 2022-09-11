dc057e0ff738c9268fcd1eb6e848bf5bf82fea07
filesync-dev <contact@filesync.pro>
2022-09-11 01:36:06 -0400

    Allows getting session data from parameter.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { UploadComponent } from './upload/upload.component';
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
